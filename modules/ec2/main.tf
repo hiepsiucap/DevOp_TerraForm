1772fb6125359d42e2b613f30729f4c5cd997fa4
bill hiep <billhiepnguyen@gmail.com>
2025-06-15 02:51:30 +0700

    Update Format

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "public" {
   ami                         = var.ami_id
   instance_type               = "t2.micro"
+  monitoring    = true
   subnet_id                   = var.public_subnet_id
   vpc_security_group_ids      = [var.public_sg_id]
   key_name                    = var.key_name
